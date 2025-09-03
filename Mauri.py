8ed42196b6630cfacddb6cf40fb6c790be839beb
a01803106-dev <a01803106@tec.mx>
2025-09-02 20:34:34 -0600

    Update Mauri.py
    tarea computacion

--- Mauri.py
+++ Mauri.py
@@ -1,1 +1,16 @@
 print("Hello World")
+
+donas = int(input("¿Cuántas donitas tienes? "))
+docenas = donas // 12
+print("Con", donas, "donas puedes hacer", docenas, "docenas!")
+
+num1 = int(input("Ingresa el primer número: "))
+num2 = int(input("Ingresa el segundo número: "))
+residuo = num1 % num2
+print(num1, "dividido entre", num2, "tiene un residuo de", residuo, "!")
+
+segundos = int(input("Ingresa un numero de segundos: "))
+horas = segundos // 3600
+minutos = (segundos % 3600) // 60
+segundos_restantes = segundos % 60
+print(segundos, "segundos son", horas, "horas,", minutos, "minutos y", segundos_restantes, "segundos")
